5e75ca56ac53d212176a9e344caf71836adc2d67
Mohammed Hamdoon <Mhamdoon4@Gmail.com>
2023-08-31 22:15:24 +0300

    changed screen size for responsivnes

--- src/utils/responsive.js
+++ src/utils/responsive.js
@@ -2,7 +2,7 @@ import { css } from "@emotion/react";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width: 415px) {
+    @media only screen and (max-width: 786px) {
       ${props}
     }
   `;
